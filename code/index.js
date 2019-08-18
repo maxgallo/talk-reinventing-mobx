70fafe6e813398df0bae2878a94fdb086e9eaf80
Max Gallo <max.gallo@dazn.com>
2019-08-18 13:07:39 +0100

    now it works in a more elegant way

--- code/index.js
+++ code/index.js
@@ -1,9 +1,9 @@
-import { observable, reaction } from 'mobx';
-// import { autorun } from 'mobx';
-import { observer } from 'mobx-react';
+import { observable, reaction, Reaction } from 'mobx';
+import { autorun } from 'mobx';
+// import { observer } from 'mobx-react'; // if I comment this one out, it doesn't work anymore :(
 import { render } from 'react-dom';
 import React, { useState, useEffect } from 'react';
-import { useCallback, memo} from "react";
+import { useCallback, memo, useRef} from "react";
 
 const album1 = observable({
     title: "OK Computer",
@@ -12,7 +12,7 @@ const album1 = observable({
 });
 
 // const autorun = func => reaction(func, func);
-// autorun(() => { console.log(`Album 1 PlayCount: ${album1.playCount}`)});
+autorun(() => { console.log(`******** PlayCount: ${album1.playCount}`)});
 
 console.log('\n ------reactions-------- \n');
 
@@ -20,65 +20,47 @@ setTimeout(() => album1.playCount = 1, 1000);
 setTimeout(() => album1.playCount = 2, 2000);
 setTimeout(() => album1.playCount = 3, 3000);
 
-//create your forceUpdate hook
-function useForceUpdate(){
-    const [value, set] = useState(true); //boolean state
-    return () => set(!value); // toggle the state to force render
-}
-
-// function useForceUpdate() {
-    // const [, setTick] = useState(0)
-
-    // const update = useCallback(() => {
-        // setTick(tick => tick + 1)
-    // }, [])
-
-    // return update
+// function useForceUpdate(){
+    // const [value, set] = useState(true); //boolean state
+    // return () => set(!value); // toggle the state to force render
 // }
 
+function useForceUpdate() {
+    const [, setTick] = useState(0)
+
+    const update = useCallback(() => {
+        setTick(tick => tick + 1)
+    }, [])
+
+    return update
+}
+
 function useObserver(functionComponent) {
+    console.log('USE OBSERVER');
     const forceUpdate = useForceUpdate();
 
-    let rendering;
-    const dispose = reaction(
-        () => {
-            rendering = functionComponent(); // this one cause a double render
-            return rendering; // if I don't return,
-        },
-        () => {
-            forceUpdate();
-        },
-    );
-    // without this one, we're adding a reaction each time
-    useEffect(() => () => dispose())
+    const reaction = useRef(null);
 
+    if (!reaction.current) {
+        reaction.current = new Reaction(`observer(pippo)`, () => {
+            forceUpdate()
+        })
+    }
+
+    let rendering;
+    reaction.current.track(() => {
+        rendering = functionComponent()
+    })
     return rendering;
 }
 
 
-function myObserver(baseComponent) {
+function observer(baseComponent) {
     const wrappedComponent = (props, ref) => {
         return useObserver(() => baseComponent(props, ref));
     }
     return wrappedComponent;
-    // const memoComponent = memo(wrappedComponent)
-    // return memoComponent;
 }
-
-
-// @observer
-// @myObserver
-// class Album extends React.Component {
-    // render() {
-        // console.log('I am rendering...');
-        // return (
-            // <div>
-                // <h1>{album1.title}</h1>
-                // <h1>{album1.playCount}</h1>
-            // </div>
-        // )
-    // }
-// }
 
 const Album = () => {
     console.log('render');
@@ -90,8 +72,8 @@ const Album = () => {
     )
 }
 
-// const ObserverAlbum = observer(Album);
-const ObserverAlbum = myObserver(Album);
+const ObserverAlbum = observer(Album);
+// const ObserverAlbum = myObserver(Album);
 
 render(
     <ObserverAlbum />,
