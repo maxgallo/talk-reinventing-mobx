1f25a9fa738a5e5edc28547823cd99059d5558e2
Max Gallo <max.gallo@dazn.com>
2019-09-01 18:29:35 +0100

    first implementation from scratch done

--- code/index1.js
+++ code/index1.js
@@ -1,26 +1,19 @@
-import { render } from 'react-dom';
-import React from 'react';
-
-// import { observer } from 'mobx-react';
-// import { observable, autorun } from 'mobx';
-import { useState, useRef, useEffect } from 'react';
-
-const album1 = observable({
-    title: "OK Computer",
-    year: 1997,
-    playCount: 0
-});
+const React = require('react');
+const { useRef, useState } = require('react');
+const { render } = require('react-dom');
+// const { observer } = require('mobx-react');
+// const { observable, autorun } = require('mobx');
 
 const accessedObservables = [];
 const derivationGraph = {};
 let count = 0;
 
-function observable(targetObject){
+function observable(targetObject) {
     const observableObject = {}
 
-    const unique = `observable(${count++})`;
-    function getObservableId(key){
-        return unique + key;
+    const unique = `observable(${count++})`
+    function getObservableId(key) {
+        unique + key;
     }
 
     Object.keys(targetObject).forEach(objectKey => {
@@ -28,91 +21,89 @@ function observable(targetObject){
             observableObject,
             objectKey,
             {
-                get(){
+                get() {
                     accessedObservables.push(getObservableId(objectKey))
                     return targetObject[objectKey];
                 },
-                set(value){
+                set(value) {
                     targetObject[objectKey] = value;
-                    derivationGraph[getObservableId(objectKey)].forEach(runner => {
-                        runner()
-                    })
+                    derivationGraph[getObservableId(objectKey)].forEach(runner => runner());
                 }
             }
         )
     })
+
     return observableObject;
 }
 
-function Reaction(name, onInvalidate){
+function Reaction(onInvalidate){
     return {
         track: trackFunction => {
             accessedObservables.length = 0;
             trackFunction();
             accessedObservables.forEach(objectId => {
-                derivationGraph[objectId] = derivationGraph[objectId] || new Set();
+                derivationGraph[objectId] = derivationGraph[objectId] || new Set()
                 derivationGraph[objectId].add(onInvalidate);
-            });
+            })
         }
     }
 }
 
-function autorun(runner) {
-    const reaction = new Reaction(`Autorun(${runner.name})`, runner);
+function autorun(runner){
+    const reaction = new Reaction(runner);
     reaction.track(runner);
 }
 
-function useForceUpdate(){
-    const [,set] = useState(0);
-    return () => set(x => !x);
+function useForceUpdate() {
+    const [,set] = useState(0)
+    return () => set(x => !x)
 }
 
-function useObserver(functionComponent) {
+function applyObserver(functionComponent) {
     const forceUpdate = useForceUpdate();
 
     const reaction = useRef(null);
 
-    if (!reaction.current) {
-        reaction.current = new Reaction(
-            'observer(pippo)',
-            () => forceUpdate()
-        )
+    if(!reaction.current) {
+        reaction.current = new Reaction(() => forceUpdate())
     }
 
-    // const dispose = () => reaction.current && reaction.current.dispose();
-    // useEffect(() => () => dispose());
-
-    let rendering;
+    let rendering
     reaction.current.track(() => {
-        rendering = functionComponent()
+        return rendering = functionComponent();
     })
+
     return rendering;
 }
 
-function observer(baseComponent) {
+function observer(component) {
     const wrappedComponent = (props, ref) => {
-        return useObserver(() => baseComponent(props, ref));
+        return applyObserver(() => component(props, ref))
     }
     return wrappedComponent;
 }
 
-autorun(() => { console.log(`******** PlayCount: ${album1.playCount}`)});
+const album1 = observable({
+    title: "OK Computer",
+    year: 1997,
+    playCount: 0
+});
 
-console.log('\n ------reactions-------- \n');
+autorun(() => { console.log(`**** PlayCount: ${album1.playCount}`)});
+
+console.log('\n------reactions-------- \n');
 
 setTimeout(() => album1.playCount = 1, 1000);
 setTimeout(() => album1.playCount = 2, 2000);
 setTimeout(() => album1.playCount = 3, 3000);
 
-const Album = () => {
-    console.log('render');
-    return (
-        <div>
-            <h1>{album1.title}</h1>
-            <h1>{album1.playCount}</h1>
-        </div>
-    )
-}
+
+const Album = () => (
+    <div>
+        <h2>{album1.title}</h2>
+        <h2>{album1.playCount}</h2>
+    </div>
+)
 
 const ObserverAlbum = observer(Album);
 
@@ -120,3 +111,5 @@ render(
     <ObserverAlbum />,
     document.getElementById('root')
 );
+
+
