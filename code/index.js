40e8105e8cf184dd14c49a65ade532dc1945cc19
Max Gallo <max.gallo@dazn.com>
2019-09-06 23:01:29 +0100

    rename to album without the 1

--- code/index.js
+++ code/index.js
@@ -1,15 +1,15 @@
 const { observable, autorun } = require('mobx')
 
-const album1 = observable({
+const album = observable({
     title: "OK Computer",
     year: 1997,
     playCount: 0
 });
 
-autorun(() => { console.log(`** count: ${album1.playCount}`)});
+autorun(() => { console.log(`** count: ${album.playCount}`)});
 
 console.log('\n---reactions---\n');
 
-setTimeout(() => album1.playCount = 1, 1000);
-setTimeout(() => album1.playCount = 2, 2000);
-setTimeout(() => album1.playCount = 3, 3000);
+setTimeout(() => album.playCount = 1, 1000);
+setTimeout(() => album.playCount = 2, 2000);
+setTimeout(() => album.playCount = 3, 3000);
